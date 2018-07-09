6d941d0f98985f50e1bf00510dcec4a1dff64141
dingxinyi <dingxinyi@geekfinancer.com>
2018-07-09 14:04:02 +0800

    initialize setup module and basic routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SetupComponent } from './pages/setup/setup.component';
+
+const routes: Routes = [
+  { path: 'setup', component: SetupComponent },
+  // TODO: 这里 redirectTo 加不加 / 有什么区别？ 暂时来看没啥区别
+  { path: '', redirectTo: '/setup', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,20 +9,23 @@ import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
 import { registerLocaleData } from '@angular/common';
 import zh from '@angular/common/locales/zh';
 
+import { AppRoutingModule } from './app-routing.module';
+import { SetupModule } from './pages/setup/setup.module';
+
 registerLocaleData(zh);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
-    NgZorroAntdModule
+    NgZorroAntdModule,
+    AppRoutingModule,
+    SetupModule
   ],
   providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
