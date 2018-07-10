ec91526464273c5b09ed93d48eae0b60cd154d8c
baishusama <baishusama@outlook.com>
2018-07-10 08:28:26 +0800

    initialize setup page's styles

--- src/app/pages/setup/setup.module.ts
+++ src/app/pages/setup/setup.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { SetupComponent } from './setup.component';
 
 @NgModule({
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, NgZorroAntdModule],
   declarations: [SetupComponent]
 })
 export class SetupModule {}
