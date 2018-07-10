d2eb930093748b13eb716b6417ba32aaa236898f
dingxinyi <dingxinyi@geekfinancer.com>
2018-07-10 10:55:41 +0800

    initialize main module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,9 @@ import { SetupComponent } from './pages/setup/setup.component';
 
 const routes: Routes = [
   { path: 'setup', component: SetupComponent },
+  // { path: 'main', component: SetupComponent },
   // TODO: 这里 redirectTo 加不加 / 有什么区别？ 暂时来看没啥区别
-  { path: '', redirectTo: '/setup', pathMatch: 'full' }
+  { path: '', redirectTo: '/main', pathMatch: 'full' }
 ];
 
 @NgModule({
